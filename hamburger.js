ef88f33aa455b70e7361cc0c3bbd579c3d6f6119
Meenaji21 <sourabhmeena0007@gmail.com>
2022-07-31 17:38:39 +0530

    portfolio: update menu icon

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,15 @@
+function myFunction(x) {
+    x.classList.toggle(".hamburger");
+}
 document.querySelector('.cross').style.display = 'none';
 document.querySelector('.hamburger').addEventListener("click", ()=>{
     document.querySelector('.sidebar').classlist.toggle('.sidebargo');
     if(document.querySelector('.sidebar').classlist.contains('.sidebargo')){
-        document.querySelector('.ham').style.display = 'inline';
-        document.querySelector('.cross').style.display = 'none';
-    }
-    else{
         document.querySelector('.ham').style.display = 'none';
         document.querySelector('.cross').style.display = 'inline';
+    }
+    else{
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
     }
 })
