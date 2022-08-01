cb7fec927526c1911b078fddb46b7d8f0114f189
Meenaji21 <sourabhmeena0007@gmail.com>
2022-08-01 18:47:46 +0530

    portfolio: update menu button

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,16 @@
-// function  sam(){
-//     let x= document.getElementById('id1');
-//     x.style.display: 'block';
-
-// }
+function sam() {
+  let x = document.getElementById('id1');
+  x.style.display = 'block';
+  let  y = document.getElementById('ham1');
+  y.style.display = 'none';
+  let z = document.getElementById("cross1");
+  z.style.display = 'block';
+}
+function sam1() {
+  let x = document.getElementById('id1');
+  x.style.display = 'none';
+  let  y = document.getElementById('ham1');
+  y.style.display = 'block';
+  let z = document.getElementById("cross1");
+  z.style.display = 'none';
+}
