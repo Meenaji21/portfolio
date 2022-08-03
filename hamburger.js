a28137d4af675bb58f52a8fb304018ee8ba4012b
Meenaji21 <sourabhmeena0007@gmail.com>
2022-08-03 17:58:30 +0530

    portfolio: add read more function in blog.html

--- hamburger.js
+++ hamburger.js
@@ -24,6 +24,6 @@ parentContainer.addEventListener('click', event=>{
   const isReadMoreBtn = current.className.includes('read-more-btn');
   if(!isReadMoreBtn) return;
   const currentText = event.target.parentNode.querySelector('.read-more-text');
-  currentText.classList.toggle('read-more-text-show');
+  currentText.classList.toggle('read-more-text--show');
   current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More..."
 })
