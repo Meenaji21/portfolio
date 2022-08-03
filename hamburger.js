3b2be302318120ea147f8cef113664319d214600
Meenaji21 <sourabhmeena0007@gmail.com>
2022-08-03 17:55:20 +0530

    portfolio: adjust font size

--- hamburger.js
+++ hamburger.js
@@ -18,3 +18,12 @@ function sam1() {
 
 // blog.html javascript
 
+const parentContainer = document.querySelector('.blog');
+parentContainer.addEventListener('click', event=>{
+  const current = event.target;
+  const isReadMoreBtn = current.className.includes('read-more-btn');
+  if(!isReadMoreBtn) return;
+  const currentText = event.target.parentNode.querySelector('.read-more-text');
+  currentText.classList.toggle('read-more-text-show');
+  current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More..."
+})
